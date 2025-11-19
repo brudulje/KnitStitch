47e1d2e7acdbb8f9864a1253d32b8986cfb7719c
Joachim Seland Graff <Joachim.Seland.Graff@gmail.com>
2025-11-19 21:35:58 +0100

    Initial Commit

---
+++ KnitStitch/KnitStitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnitStitchApp.swift
+//  KnitStitch
+//
+//  Created by Joachim Seland Graff on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct KnitStitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
